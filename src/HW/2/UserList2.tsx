58645c41373ca8176c6fceb47bf55049a86d9ac3
irina-snezan <irina-snezan@tut.by>
2023-10-31 03:18:20 +0300

    Add solution hw02

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -1,23 +1,31 @@
 import {CurrentUser} from './CurrentUser';
-import {UsersObjectType} from './HW2';
+import {UsersObjectType, UserType} from './HW2';
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+	users: UsersObjectType // пропиши типизацию+
+	filterUsers: () => void // пропиши типизацию
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
+
+	const onClickHandler = () => {
+		props.filterUsers()
+	}
+
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+			<button id={'hw02-filter-button'} onClick={onClickHandler}>SHOW ME FRIENDS FROM LA</button>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
+			<ul>
+			  {props.users.myFriends.map((user: UserType, index) => {
+				  return <li key={index}>
+					  <CurrentUser user={user}/>
+				  </li>
+				  }
+			  )}
+			</ul>
 		</div>
 	);
 };
